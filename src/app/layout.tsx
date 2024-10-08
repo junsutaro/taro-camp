be63bb2fe5c5b151324b82709a827adc0dc859a8
Junsu <s2s2s2x101@gmail.com>
2024-10-08 15:56:58 +0900

    favicon-2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import '../../public/favicon.png'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
