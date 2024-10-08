2ca852036136bcb304b1492cdc4f23a5b57c6d1d
Junsu <s2s2s2x101@gmail.com>
2024-10-08 16:46:25 +0900

    환경변수 편집

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,15 +7,6 @@ import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCOC1_wZfNNbMZ_bwmfkm_yfnqK7a5Kxxg',
-//   authDomain: 'taro-camp.firebaseapp.com',
-//   projectId: 'taro-camp',
-//   storageBucket: 'taro-camp.appspot.com',
-//   messagingSenderId: '261214551558',
-//   appId: '1:261214551558:web:7352a73fbc1767ea784d87',
-//   measurementId: 'G-KN7F2KTWWJ',
-// };
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
