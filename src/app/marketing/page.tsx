d108ec5e7e7ec5559b17f7a68c481113fac5ff09
Junsu <s2s2s2x101@gmail.com>
2024-10-09 20:20:49 +0900

    pr체크1

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -3,6 +3,7 @@ export default function Marketing() {
     <div className="text-center">
       <h1 className="text-4xl font-bold"> Marketing Trends </h1>
       <p>Share insights</p>
+      <p>PR테스트</p>
     </div>
   );
 }
