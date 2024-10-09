bf941386c45477a6a68424e572815cd510a0244a
Junsu <s2s2s2x101@gmail.com>
2024-10-10 02:56:54 +0900

    diary 대 개편

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,47 @@
 // src/app/page.tsx
 
-import Link from 'next/link';
+'use client';
 
-export default function Home() {
+import {useEffect, useState} from 'react';
+
+// 유튜브 링크를 저장한 배열
+const youtubeLinks = [
+  'https://www.youtube.com/embed/ddl_m1hwWO4', // Destiny Rogers - Outside
+  'https://www.youtube.com/embed/H4eTCeWGgVQ', // 심규선 my little bird
+  'https://www.youtube.com/embed/BGn2oo-0Dqc', // Gorillaz - On Melancholy Hill
+  'https://www.youtube.com/embed/8evbtRNtz8s', // Snoop Lion - Ashtrays and Heartbreaks ft. Miley Cyrus
+  'https://www.youtube.com/embed/VQZXXciZb_c', // 10CM - Gradation(그라데이션)
+];
+
+export default function MainPage() {
+  const [randomLink, setRandomLink] = useState<string>('');
+
+  useEffect(() => {
+    // 배열에서 랜덤한 링크를 가져오기
+    const randomIndex = Math.floor(Math.random() * youtubeLinks.length);
+    setRandomLink(youtubeLinks[randomIndex]);
+  }, []);
+
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold">Taro Camp</h1>
-      <nav className="mt-4">
-        <Link href={'/diary'} className="text-blue-400">
-          Go to Diary
-        </Link>{' '}
-        <br />
-        <Link href={'/marketing'} className="text-blue-400">
-          Go to Marketing
-        </Link>
-      </nav>
+    <div
+      className="main-page min-h-screen flex flex-col items-center bg-cover bg-center"
+      style={{
+        backgroundImage: `url('/background.jpg')`,
+      }}>
+      {' '}
+      <h1 className="text-3xl font-bold text-center mb-8">랜덤픽</h1>
+      {randomLink && (
+        <div className="youtube-video-container flex justify-center items-center">
+          <iframe
+            width="700"
+            height="550"
+            src={randomLink}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen></iframe>
+        </div>
+      )}
     </div>
   );
 }
