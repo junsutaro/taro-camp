42da68e88eaa447573c480292a824256381e0304
Junsu <s2s2s2x101@gmail.com>
2024-10-09 20:47:12 +0900

    master브랜치로 푸쉬용

--- src/app/marketing/page.tsx
+++ src/app/marketing/page.tsx
@@ -3,10 +3,6 @@ export default function Marketing() {
     <div className="text-center">
       <h1 className="text-4xl font-bold"> Marketing Trends </h1>
       <p>Share insights</p>
-      <p>PR테스트</p>
-      <p>PR테스트2</p>
-      <p>PR테스트3</p>
-      <p>PR테스트4</p>
     </div>
   );
 }
